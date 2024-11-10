21e2eb5c374094e0387e533144acb515ef803404
Marquis Huffman <marquishuffman1@gmail.com>
2024-11-10 14:17:17 -0500

    Ceaser Cypher Solution

--- cipher.py
+++ cipher.py
@@ -1,1 +1,15 @@
-# add your code here
+abc = "abcdefghijklmnoplqrstuvwxyz"
+shift = 5
+text = input("Please enter a sentence: ")
+text = text.lower()
+
+encrypt = ""
+
+for char in text:
+    if char in abc: 
+        index = abc.find(char)
+        index = (index + shift) % 26
+        char = abc[index]
+    encrypt += char
+
+print("The encrypted message is", encrypt)
